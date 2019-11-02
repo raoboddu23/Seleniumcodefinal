f908cbd0db3489b77582457b412b4665b762d3e9
ravilella1234 <ravilella1234>
2019-11-02 08:44:25 +0530

    added project

---
+++ NovemberBatch2019/src/com/launch/TC_001.java
@@ -0,0 +1,45 @@
+package com.launch;
+
+import org.openqa.selenium.By;
+
+public class TC_001 extends BaseTest
+{
+
+	public static void main(String[] args) throws Exception 
+	{
+		init();
+		
+		launch("chromebrowser");
+		
+		navigateUrl("hdfcurl");
+		
+		driver.manage().window().maximize();
+		
+		/*String title = driver.getTitle();
+		System.out.println(title);
+		
+		String url = driver.getCurrentUrl();
+		System.out.println(url);
+		
+		driver.manage().deleteAllCookies();
+		
+		driver.navigate().back();
+		
+		Thread.sleep(4000);
+		
+		driver.navigate().forward();
+		
+		Thread.sleep(4000);
+		
+		driver.navigate().refresh();*/
+		
+		driver.findElement(By.linkText("KNOW MORE")).click();
+		
+		//driver.close();
+		
+		driver.quit();
+		
+			
+	}
+
+}
